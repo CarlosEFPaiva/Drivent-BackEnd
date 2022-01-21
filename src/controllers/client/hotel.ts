93f24a4c6c9ae93ab07c432555e1565402fee498
liggia <liggia.cruz@gmail.com>
2022-01-21 08:41:08 -0300

    testing

--- src/controllers/client/hotel.ts
+++ src/controllers/client/hotel.ts
@@ -4,5 +4,6 @@ import * as service from "@/services/client/hotel";
 
 export async function getHotels(req: Request, res: Response) {
   const hotelInfo = await service.getHotelsInfo();
+  console.log("oi");
   res.send(hotelInfo);
 }
