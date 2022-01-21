dc05f1fd4f0cb69d3bf163455abf30fe343cedd8
liggia <liggia.cruz@gmail.com>
2022-01-21 08:44:48 -0300

    delete: console.log

--- src/controllers/client/hotel.ts
+++ src/controllers/client/hotel.ts
@@ -4,6 +4,5 @@ import * as service from "@/services/client/hotel";
 
 export async function getHotels(req: Request, res: Response) {
   const hotelInfo = await service.getHotelsInfo();
-  console.log("oi");
   res.send(hotelInfo);
 }
