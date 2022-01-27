1c9c687fb830c0fe7b456270d2c198f7580db8ec
CarlosEFPaiva <carlosedufpaiva@poli.ufrj.br>
2022-01-27 11:30:08 -0300

    refac: removed commented lines and optimized talksService order of repository requests

--- src/services/client/talks.ts
+++ src/services/client/talks.ts
@@ -5,15 +5,19 @@ import ConflictError from "@/errors/ConflictError";
 import NotFoundError from "@/errors/NotFoundError";
 
 export async function subscribe(userId: number, eventId: number) {
-  const user = await User.findOne({ id: userId });
   const eventToBeSubscribed = await Event.findOne({ id: eventId });
+
   if (!eventToBeSubscribed) {
     throw new NotFoundError();
   }
+
+  const user = await User.findOne({ id: userId });
   const subscribedEvent = await eventToBeSubscribed.findConflictingTalks(user);
+
   if (subscribedEvent) {
     throw new ConflictError("There are other talks scheduled for the same time");
   }
+  
   await UserEvent.createSubscription(user, eventToBeSubscribed);
   return;
 }
